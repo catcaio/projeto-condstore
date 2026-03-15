de49446e47d496ec36ba08ab2db6798cc52b6d8f
catcaio <catcaio@users.noreply.github.com>
2026-03-15 16:11:55 -0300

    chore(pr120): complete removal of governance code and update routes registry

--- src/app/(app)/cockpit/governance/[spaceSlug]/[projectSlug]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ProjectClientView } from '@/modules/governance/components/project-client-view';
-
-export default async function ProjectPage({ params }: { params: Promise<{ spaceSlug: string, projectSlug: string }> }) {
-    const { spaceSlug, projectSlug } = await params;
-    return <ProjectClientView spaceSlug={spaceSlug} projectSlug={projectSlug} />;
-}
